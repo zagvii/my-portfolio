9d4b13005e30ed3a89925aec29f118eda167cfaa
zagvii <mirian.vieira.moreira@gmail.com>
2025-09-04 21:17:01 -0300

    fixJsxFile

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import '../css/style.css'; 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './i18n/index.js';
