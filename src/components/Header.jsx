4f8070df0aecb104559bae57725c8728c65b05cd
zagvii <mirian.vieira.moreira@gmail.com>
2026-01-12 22:34:08 -0300

    header and typing effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,53 @@
-import React from 'react';
-import '../../css/Header.css'; 
+import React, { useState }from 'react';
+import '../../css/Header.css';
 
+import { Moon, Sun } from 'lucide-react';
 import { useTranslation } from "react-i18next";
 
 function Header () {
     const { t, i18n } = useTranslation();
 
-    const changeLanguage = (event) => {
-        const selectedLang = event.target.value;
-        i18n.changeLanguage(selectedLang);
-        localStorage.setItem("lang", selectedLang);
+    const [currentLang, setCurrentLang] = useState(localStorage.getItem("lang") || "en");
+    const [theme, setTheme] = useState('dark');
+
+    const handleLanguageChange = (newLang) => {
+        i18n.changeLanguage(newLang);
+        setCurrentLang(newLang);
+
+        localStorage.setItem("lang", newLang);
     };
+
+    const ThemeSwitch = () => {
+        const [isDarkMode, setIsDarkMode] = useState(true); 
+        const handleToggle = () => {
+            setIsDarkMode(!isDarkMode);
+        };
+    }
 
     return (
         <header>
-            <nav>
-                <ul>
-                    <li><a href="#about-me-section">{t("aboutMe")}</a></li>
-                    <li><a href="#education-section">{t("education")}</a></li>
-                    <li><a href="#experiences-section">{t("workExperiences")}</a></li>
-                </ul>
-                <div className='rightAlign'>
-                    <a className="resume-button-header" href={`${import.meta.env.BASE_URL}assets/resume-EN.pdf`} download>{t("downloadMyCV")}</a>
-                    <select
-                        className='selectLanguage'
-                        onChange={changeLanguage}
-                        defaultValue={localStorage.getItem("lang") || "pt"} >
-                        <option value="pt">🇧🇷 PT</option>
-                        <option value="en">🇺🇸 EN</option>
-                    </select>
-                </div>
-            </nav>
+            <p>PORTFOLIO</p>
+            <label className="switchTheme">
+                <input 
+                    type="checkbox" 
+                />
+                <span className="roundSwitch">
+                    <Moon className="icon-moon" size={16} />
+                    <Sun className="icon-sun" size={16} />
+                </span>
+            </label>
+
+            <div className="glass-red-container">
+                <button className={`glass-lang-item ${currentLang === 'pt' ? 'active' : ''}`} onClick={() => handleLanguageChange('pt')} >
+                    <span className="flag-icon">🇧🇷</span>
+                    <span className="lang-text">PT</span>
+                </button>
+
+                <button className={`glass-lang-item ${currentLang === 'en' ? 'active' : ''}`} onClick={() => handleLanguageChange('en')} >
+                    <span className="flag-icon">🇺🇸</span>
+                    <span className="lang-text">EN</span>
+                </button>
+            </div>
         </header>
       );
 }
